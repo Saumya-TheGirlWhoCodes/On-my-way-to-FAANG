f5f3d943b17c69b053aff7cc1fd410ebc7dd475b
Saumya Srivastava <51478376+Saumya-TheGirlWhoCodes@users.noreply.github.com>
2021-08-01 22:39:35 +0530

    LeetCode Problem 1920

---
+++ Array/Easy/BuildArrayFromPermutation.java
@@ -0,0 +1,12 @@
+/* LeetCode Problem 1920 */
+
+class Solution {
+    public int[] buildArray(int[] nums) {
+        int n=nums.length;
+        int ans[]=new int[n];
+        for(int i=0;i<n;i++){
+            ans[i]=nums[nums[i]];
+        }
+        return ans;
+    }
+}
